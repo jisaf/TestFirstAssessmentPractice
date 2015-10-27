1e706249ae3ec79c15dd881d20aeef2c5fec27e2
Scott D'Alessandro <sdaless22@gmail.com>
2015-10-27 19:02:47 -0400

    Update event_emitter.js

--- 03_event_emitter/event_emitter.js
+++ 03_event_emitter/event_emitter.js
@@ -1,17 +1,1 @@
-function MyEventEmitter(){
-  this.events = {};
-}
 
-MyEventEmitter.prototype.addListener = function(name, cb){
-  if(this.events[name] === undefined){
-    this.events[name] = [cb];
-  } else {
-    this.events[name].push(cb);
-  }
-}
-
-MyEventEmitter.prototype.emit = function(name, arg) {
-  this.events[name].forEach(function(index){
-    index.apply(this, [arg]);
-  });
-}
