36d8b7a7bc8d8493e7d8c66fee89a73f1e09a304
Scott D'Alessandro <sdaless22@gmail.com>
2015-10-27 19:02:30 -0400

    Update sometimes.js

--- 01_sometimes/sometimes.js
+++ 01_sometimes/sometimes.js
@@ -1,10 +1,1 @@
-function sometimes(fn) {
-  var i = 0
-  return function() {
-    if (++i >= 4 && (i & 1) === 0) {
-      return "I do not know!"
-    }
-    
-    return fn.apply(null, arguments)
-  }
-}
+
